30b2691f13bf4dac1334cd8b6ca8a90d6b3f070c
ahinojo1 <ahinojo1@mail.depaul.edu>
2018-01-16 12:29:36 -0600

    Added battleship interface
    Changed battleship class to implement interface

--- SE350/src/battleshipAssignment/Battleship.java
+++ SE350/src/battleshipAssignment/Battleship.java
@@ -1,5 +1,16 @@
 package battleshipAssignment;
 
-public class Battleship {
-
+public class Battleship implements BattleSearchStrategy {
+	public Battleship() {
+		
+	}
+	public void insertBattleInfo(String fileName) {
+		
+	}
+	@Override
+	public void search() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
