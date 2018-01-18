c32894ca685ed30fedd45e6a6b7b7e0c723856a8
ahinojo1 <ahinojo1@mail.depaul.edu>
2018-01-17 23:54:26 -0600

    Starting Battleship

---
+++ SE350/src/battleshipAssignment/HorizontalSearch.java
@@ -0,0 +1,11 @@
+package battleshipAssignment;
+
+public class HorizontalSearch implements BattleSearchStrategy {
+
+	@Override
+	public void search() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ SE350/src/battleshipAssignment/RandomSearch.java
@@ -0,0 +1,11 @@
+package battleshipAssignment;
+
+public class RandomSearch implements BattleSearchStrategy {
+
+	@Override
+	public void search() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
